781ac59034466383068ec6e303d538c6dc96a6cb
marcy2011 <133589298+marcy2011@users.noreply.github.com>
2025-07-22 19:40:30 +0200

    Add files via upload

---
+++ redirect_whitelist.php
@@ -0,0 +1,10 @@
+<?php
+return [
+    'https://rena.altervista.org',
+    'https://rena.altervista.org/rena.html',
+    'https://rena.altervista.org/menu.html',
+    'https://rena.altervista.org/news.php',
+    'https://rena.altervista.org/account.php',
+    'https://rena.altervista.org/chi-siamo.html',
+    'https://rena.altervista.org/privacy-policy.html',
+];
